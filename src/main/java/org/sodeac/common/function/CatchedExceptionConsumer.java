5770faec16b8a7ac56f2c8a60c407ca77c08efe9
spalarus <s.palarus@googlemail.com>
2020-01-03 19:36:56 +0100

    docs and and header

--- src/main/java/org/sodeac/common/function/CatchedExceptionConsumer.java
+++ src/main/java/org/sodeac/common/function/CatchedExceptionConsumer.java
@@ -1,11 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Sebastian Palarus
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     Sebastian Palarus - initial API and implementation
+ *******************************************************************************/
 package org.sodeac.common.function;
 
-import java.sql.SQLException;
 import java.util.function.Consumer;
 
+/**
+ * Extends {@link Consumer} to consume with potentially throws an exception. Throwed exceptins will delegate as {@link RuntimeException}
+ * 
+ * @author Sebastian Palarus
+ *
+ * @param <T>
+ */
+@FunctionalInterface
 public interface CatchedExceptionConsumer<T> extends Consumer<T>
 {
-
 	@Override
 	default void accept(T t)
 	{
@@ -23,6 +39,13 @@ public interface CatchedExceptionConsumer<T> extends Consumer<T>
 		}
 	}
 	
+	/**
+	 * Consume object with potentially throws an exception.
+	 * 
+	 * @param t object to consume 
+	 * 
+	 * @throws Exception
+	 */
 	public void acceptWithException(T t) throws Exception;
 	
 	public static <T> Consumer<T> wrap(CatchedExceptionConsumer<T> consumer)
